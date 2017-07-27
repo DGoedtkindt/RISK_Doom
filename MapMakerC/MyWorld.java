92104bd3c4521bc8714d590b05b6c1a79490ebc6
Dario Goedtkindt <dario@goedtkindt.name>
2017-07-27 19:07:41 +0200

    created a separate class for the XML must still be cleanCoded

--- MapMakerC/MyWorld.java
+++ MapMakerC/MyWorld.java
@@ -1,21 +1,7 @@
 import greenfoot.*;
-
 import java.awt.Color;
 import java.util.*;
 
-//Import les xml
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 public class MyWorld extends World
 {
@@ -116,8 +102,8 @@ public class MyWorld extends World
     
     private void testContinentChange()
     {
-
-        //testTerritory.setContinent(new Color(145,145,230));
+        //must create continent first
+        //testTerritory.setContinent(continent);
 
     }
     
@@ -127,135 +113,6 @@ public class MyWorld extends World
     public void changeMode(int newMode){
         
         currentMode = newMode;
-        
-    }
-    
-    
-    
-    public void init(){
-        
-        
-        
-    }
-    
-    
-    // Inspiré de https://www.mkyong.com/java/how-to-create-xml-file-in-java-dom/
-    public void saveToXML(){
-        
-        List continentList = getObjects(Continent.class);
-        
-        try{
-            
-            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-            
-            Document doc = docBuilder.newDocument();
-            Element rootElement = doc.createElement("map");//Création de rootElement
-            doc.appendChild(rootElement);
-            
-            
-            for(Object cContinent : continentList){//While  : append des continents à la map (et caractéristiques)
-                
-                Continent currentContinent = (Continent)cContinent;
-                
-                Element continent = doc.createElement("continent");
-                rootElement.appendChild(continent);
-                
-                List<Territory> territoriesInContinent = currentContinent.getContainedTerritories();
-                
-                for(Territory currentTerritory : territoriesInContinent){//append des territoires aux continents (et caractéristiques)
-                    
-                    Element territory = doc.createElement("territory");
-                    continent.appendChild(territory);
-                    
-                    int hexNumber = 0;
-                    
-                    ArrayList<TerritoryHex> composingHexes = currentTerritory.getComposingHex();
-                    
-                    for(TerritoryHex currentHex : composingHexes){//append des hex aux territoires
-                        
-                        Element hex = doc.createElement("hex");
-                        territory.appendChild(hex);
-                        
-                        Attr hexX = doc.createAttribute("hexX");
-                        hexX.setValue("" + currentHex.getX());
-                        hex.setAttributeNode(hexX);
-                        
-                        Attr hexY = doc.createAttribute("hexY");
-                        hexY.setValue("" + currentHex.getY());
-                        hex.setAttributeNode(hexY);
-                        
-                    }
-                    
-                    Attr capitalPoints = doc.createAttribute("capitalPoints");
-
-                    capitalPoints.setValue("" + currentTerritory.getBonusPoints());
-
-                    territory.setAttributeNode(capitalPoints);
-                    
-                    Attr territoryOwner = doc.createAttribute("territoryOwner");
-                    territoryOwner.setValue("");
-                    territory.setAttributeNode(territoryOwner);
-                    
-                    Attr armies = doc.createAttribute("territoryArmies");
-                    armies.setValue("0");
-                    territory.setAttributeNode(armies);
-                    
-                    Attr id = doc.createAttribute("territoryId");
-                    armies.setValue("" + currentTerritory.getId());
-                    territory.setAttributeNode(id);
-                    
-                    
-                    
-
-                    List<Territory> borderingTerritories = currentTerritory.getBorderTerritories();
-                    
-                    //int borderingNumber = 0;
-                    
-                    for(Territory currentBordering : borderingTerritories){//append des limitrophes aux territoires
-                        
-                        Element bordering = doc.createElement("borderingTerritory");
-                        territory.appendChild(bordering);
-                        
-                        Attr borderingID = doc.createAttribute("borderingID");
-                        borderingID.setValue("" + currentBordering.getId());
-                        bordering.setAttributeNode(borderingID);
-                        
-                    }
-                    
-                }
-                
-                
-                Attr continentPoints = doc.createAttribute("continentPoints");
-                continentPoints.setValue("" + currentContinent.getContinentColor());
-                continent.setAttributeNode(continentPoints);
-                
-                Attr rContinentColor = doc.createAttribute("rContinentColor");
-                rContinentColor.setValue("" + (currentContinent.getContinentColor()).getRed());
-                continent.setAttributeNode(rContinentColor);
-                
-                Attr gContinentColor = doc.createAttribute("gContinentColor");
-                gContinentColor.setValue("" + (currentContinent.getContinentColor()).getGreen());
-                continent.setAttributeNode(gContinentColor);
-                
-                Attr bContinentColor = doc.createAttribute("bContinentColor");
-                bContinentColor.setValue("" + (currentContinent.getContinentColor()).getBlue());
-                continent.setAttributeNode(bContinentColor);
-                
-            }
-            
-            
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("C:\\Users\\dervi\\Desktop\\PROJEEEEET\\RISK_Doom\\MapMakerC"));
-            
-        }catch(Exception e){
-            
-            System.out.println("Erreur xml" + e.getMessage());
-            
-        }
-
         
     }
     
@@ -285,7 +142,7 @@ public class MyWorld extends World
         
         mouse = Greenfoot.getMouseInfo();
         
-        if(getPressedButton() != null){ // Si on a appuyé quelque part
+        if(Greenfoot.mouseClicked(null)){ // Si on a appuyé quelque part
             
             lastClickedButton = getPressedButton();
             
