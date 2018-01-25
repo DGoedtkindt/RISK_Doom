77e8ca30c44d3bea6d2cbaf79ff56a074d37307e
DervishiD <DervishiD01@gmail.com>
2018-01-25 17:29:40 +0100

    Update Player.java

--- mainObjects/Player.java
+++ mainObjects/Player.java
@@ -1,6 +1,72 @@
 package mainObjects;
 
+import base.GColor;
 
 public class Player {
-
+    
+    private final String name;
+    private final GColor color;
+    private int armiesInHand = 0;
+    
+    private Combo combos = new Combo();
+    
+    public Player(String playerName, GColor c){
+        name = playerName;
+        color = c;
+        
+    }
+    
+    public void startTurn(){
+        
+        if(!aPlayerIsDead()){
+            getArmies();
+            
+        }
+        
+    }
+    
+    private void getArmies(){
+        
+        int n = 0;
+        
+        /*
+        
+        for(Territory t : SOMEWHERE territories.size()){
+            
+            if(t.owner == this){
+                n++;
+            }
+            
+        }
+        
+        */
+        
+        armiesInHand += Math.floor(n / 3);
+        
+    }
+    
+    static public boolean aPlayerIsDead(){
+        
+        /*
+        
+        ArrayList<Player> playersAlive = new ArrayList<Player>();
+        
+        for(Territory t : SOMEWHERE territories.size()){
+            
+            if(t.owner != null && !playersAlive.contains(t.owner())){
+                playersAlive.add(t.owner());
+            }
+            
+            if(playersAlive.size() != SOMEWHERE players.size()){
+                SOMEWHERE endGame();
+                return true;
+            }
+            
+        }
+        
+        */
+        
+        return false;
+    }
+    
 }
