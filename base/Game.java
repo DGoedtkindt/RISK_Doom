1f6b43f70838dcd44471e2409fdd0daf80338f0a
DervishiD <DervishiD01@gmail.com>
2018-02-04 15:32:21 +0100

    Add files via upload

--- base/Game.java
+++ base/Game.java
@@ -1,8 +1,11 @@
 package base;
 
 import game.Difficulty;
+import game.Turn;
+import greenfoot.Greenfoot;
 import java.util.ArrayList;
 import mainObjects.Player;
+import mainObjects.Territory;
 
 public class Game {
     /**
@@ -10,6 +13,7 @@ public class Game {
      */
     public enum State {
         HELLO("HELLO THERE!");
+        
         State(String description) {}
         
     }
@@ -18,5 +22,38 @@ public class Game {
     public Map map;
     public Difficulty difficulty;
     
+    public void start(){
+        
+        MyWorld.theWorld.load(new game.Manager(this));
+        giveTerritoriesRandomly();
+        Turn.start();
+        
+    }
+    
+    public void end(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void giveTerritoriesRandomly(){
+        
+        for(Player p : players){
+            
+            int terrNumber = 0;
+            Territory terrToAttribute;
+            
+            while(terrNumber < 1){
+                
+                terrToAttribute = map.territories.get(Greenfoot.getRandomNumber(map.territories.size()));
+                
+                if(terrToAttribute.owner() == null){
+                    terrToAttribute.setOwner(p);
+                    terrNumber++;
+                }
+                
+            }
+            
+        }
+        
+    }
     
 }
