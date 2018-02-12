e008cde38b9d70c93347a10945a5bb6c85d6bcfe
Dario Goedtkindt <dario@goedtkindt.name>
2018-02-12 13:36:47 +0100

    refactored the method to save a map

--- mapEditor/Manager.java
+++ mapEditor/Manager.java
@@ -1,31 +1,17 @@
 package mapEditor;
 
 //<editor-fold defaultstate="collapsed" desc="Imports">
-import appearance.Theme;
 import base.Action;
 import base.Map;
-import base.MyWorld;
 import base.NButton;
 import base.StateManager;
 import mode.Mode;
 import mode.ModeMessageDisplay;
-import greenfoot.GreenfootImage;
 import greenfoot.Actor;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-import javax.swing.Box;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 import mainObjects.Continent;
 import mainObjects.Links;
 import mainObjects.Territory;
-import mapXML.MapXML;
 import selector.Selector;
 //</editor-fold>
 
@@ -40,7 +26,7 @@ public class Manager extends StateManager{
     
     public Manager(Map loadMap) {
         mapToLoad = loadMap;
-        ctrlPanel = new ControlPanel();
+        ctrlPanel = new ControlPanel(this);
         modeDisp = new ModeMessageDisplay();
         options = new NButton(loadOptionsMenu, "Options");
     
@@ -110,80 +96,11 @@ public class Manager extends StateManager{
     
     //Action pour sauver le monde
     
-    protected static Action saveFile = () -> {
-        
-        String mapName = "";
-        String mapDescription = "";
-        
-        //créer la boite de dialogue
-        JTextField nameField = new JTextField(15);
-        JTextField descriptionField = new JTextField(65);
-        JPanel questionPanel = new JPanel();
-        questionPanel.add(new JLabel("Name : "));
-        questionPanel.add(nameField);
-        questionPanel.add(Box.createHorizontalStrut(20));
-        questionPanel.add(new JLabel("Description : "));
-        questionPanel.add(descriptionField);
-
-        //get le résultat de la boite de dialogue
-        int output = JOptionPane.showConfirmDialog(null, questionPanel, "Give your map a name and a description", JOptionPane.OK_CANCEL_OPTION);
-        if (output == JOptionPane.OK_OPTION) {
-           mapName = nameField.getText();
-           mapDescription = descriptionField.getText();
-        }
-
-        //vérifier quelques conditions
-        int writeQ = JOptionPane.YES_OPTION;
-
-        if(mapName == null || mapName.isEmpty() || mapName == "New Map") {
-            System.err.println("You can't save a map if it has no name or if its name is 'New Map'");
-            writeQ = JOptionPane.NO_OPTION;
-
-        } else if(new File("Maps/"+mapName+".xml").exists()){
-                writeQ = JOptionPane.showConfirmDialog(
-                        null, "Do you want to replace the existing map '" + mapName + "' with this one?", 
-                        "Replacing an existing map", JOptionPane.YES_NO_OPTION);
-
-        }
-
-        //si les conditions sont accomplies: sauver
-        if(writeQ == JOptionPane.YES_OPTION){
-            try {
-                MapXML xml = new MapXML(MyWorld.theWorld.stateManager.map());
-                xml.write(mapName, mapDescription);
-                
-                //écrivre l'image du thumbnail (solution temporaire)
-                try{
-                    BufferedImage mapImage = MyWorld.theWorld.getBackground().getAwtImage();
-                    File out = new File(new File("Maps").getAbsolutePath() + "/" + mapName + ".png");
-
-                    ImageIO.write(mapImage, "PNG", out);
-                    System.out.println("thumbnail saved");
-
-                } catch(IOException ex) {
-                    System.err.println("thumbnail couldn't be saved : " + ex);
-
-                }
-
-            } catch (Exception ex) {
-                System.err.println("Map could not be saved : "+ ex);
-            }
-
-        }
+    protected Action saveMap = () -> {
+        ( new MapSaver( map() ) ).saveMap();
     };
     
     /////Private Methods///////////////////////
-    
-    private static JPanel makeDialogBox() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    private void paintBackgroundDarker() {
-        GreenfootImage bckGrd = world().getBackground();
-        bckGrd.setColor(Theme.used.backgroundColor.darker());
-        bckGrd.fill();
-    
-    }
     
     private Action loadOptionsMenu = () -> {
                 clearScene();
---
+++ mapEditor/MapSaver.java
@@ -0,0 +1,124 @@
+package mapEditor;
+
+
+import base.Map;
+import base.MyWorld;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import mapXML.MapXML;
+
+public class MapSaver {
+    private Map map;
+    private String name;
+    private String description;
+    
+    public MapSaver(Map mapToSave) {
+        map = mapToSave;
+    
+    }
+    
+    public void saveMap() {
+        askForNameAndDescription();
+        if(name != null) {
+            if(!nameIsValid()) {
+                showNameError();
+            } else if(nameAlreadyExists()) {
+                if(confirm()) save();
+            } else save();
+        }
+    };
+    
+    private void askForNameAndDescription() {
+        NamePanel namePanel = new NamePanel();
+        int nameEntered = JOptionPane.showConfirmDialog(null, namePanel, "Give your map a name and a description", JOptionPane.OK_CANCEL_OPTION);
+        if(nameEntered == JOptionPane.OK_OPTION) {
+            name = namePanel.name();
+            description = namePanel.description();
+        };
+    
+    }
+    
+    private boolean nameIsValid() {
+        return !(name == null || name.isEmpty() || name == "New Map");
+    
+    }
+    
+    private void showNameError() {
+        JOptionPane.showMessageDialog(null, "You can't save a map if it has no name or if its name is 'New Map'");
+        
+    }
+    
+    private boolean nameAlreadyExists() {
+        return new File("Maps/"+name+".xml").exists();
+    
+    }
+    
+    private boolean confirm() {
+        int answer = JOptionPane.showConfirmDialog(
+                        null, "Do you want to replace the existing map '" + name + "' with this one?", 
+                        "Replacing an existing map", JOptionPane.YES_NO_OPTION);
+        return answer == JOptionPane.YES_OPTION;
+    
+    }
+    
+    private void save() {
+        try {
+            saveXML();
+            try {
+                saveThumbnail();
+            } catch (Exception ex) {
+                System.err.println("thumbnail couldn't be saved : " + ex);
+            }
+            
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "the Map could not be saved : " + ex);
+        }
+    
+    }
+    
+    private void saveXML() throws Exception{
+        MapXML xml = new MapXML(map);
+        xml.write(name, description);
+    
+    }
+    
+    private void saveThumbnail() throws Exception {
+        BufferedImage mapImage = MyWorld.theWorld.getBackground().getAwtImage();
+        File out = new File(new File("Maps").getAbsolutePath() + "/" + name + ".png");
+
+        ImageIO.write(mapImage, "PNG", out);
+        System.out.println("thumbnail saved");
+    
+    }
+    
+    private static class NamePanel extends JPanel {
+        JTextField nameField = new JTextField(15);
+        JTextField descriptionField = new JTextField(65);
+        
+        NamePanel() {
+            this.add(new JLabel("Name : "));
+            this.add(nameField);
+            this.add(Box.createHorizontalStrut(20));
+            this.add(new JLabel("Description : "));
+            this.add(descriptionField);
+        
+        }
+        
+        String name() {
+            return nameField.getText();
+        
+        }
+        
+        String description() {
+            return descriptionField.getText();
+        
+        }
+    
+    }
+}
