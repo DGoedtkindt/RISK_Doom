40f65c9876013eb1c59aecf1feacb427993aa3f7
DervishiD <DervishiD01@gmail.com>
2018-03-19 20:52:11 +0100

    Add files via upload

--- xmlChoosers/GameChooser.java
+++ xmlChoosers/GameChooser.java
@@ -1,10 +1,20 @@
 package xmlChoosers;
 
+import appearance.MessageDisplayer;
 import base.Game;
 import gameXML.GameXML;
 import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
-/** XMLChooser for Games.
+/** 
+ * XMLChooser for Games.
  */
 public class GameChooser extends XMLChooser{
     private final static String DIRECTORY_NAME = "Games";
@@ -15,7 +25,35 @@ public class GameChooser extends XMLChooser{
 
     @Override
     protected String getDescription(String fileName) {
-        return "Method getDescription() in class GameChooser is not supported yet";
+        String mapDescription = "";
+        Document doc;
+        
+        try{
+        
+            File XMLFile = new File(fileName);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(XMLFile);
+            doc.getDocumentElement().normalize();
+            
+            Element mapElement = ((Element)(doc.getElementsByTagName("game").item(0)));
+            
+            if(mapElement.hasAttribute("description")){
+                mapDescription = mapElement.getAttribute("description");
+                
+            }
+            
+        }catch(IOException | ParserConfigurationException | DOMException | SAXException ex){
+            String message = "Couldn't get the Game's description";
+            MessageDisplayer.showException(new Exception(message, ex));
+            return null;
+        }
+        
+        if(!mapDescription.equals("")){
+            return mapDescription;
+        }else{return "This Game has no description";}
+        
+
     }
 
     public Game getSelectedGame() throws Exception{
