4a9fc4c7ce055faaed47d29be9bb96a211ea3791
DervishiD <DervishiD01@gmail.com>
2018-03-20 18:31:55 +0100

    Add files via upload

--- game/NextTurnPanel.java
+++ game/NextTurnPanel.java
@@ -23,10 +23,10 @@ public class NextTurnPanel {
     private final Player OWNER;
     private final Turn TURN;
     
-    private TurnStat stats;
+    private final TurnStat STATS;
     
-    private NButton saveButton;
-    private NButton panel;
+    private final NButton SAVE_BUTTON;
+    private final NButton PANEL;
 
     /**
      * Creates a NextTurnPanel.
@@ -35,13 +35,13 @@ public class NextTurnPanel {
     public NextTurnPanel(Turn turn){
         TURN = turn;
         OWNER = TURN.player;
-        saveButton = new NButton(showSaveGameDialog,"Save Game");
-        panel = new NButton(() -> {
+        SAVE_BUTTON = new NButton(SHOW_SAVE_GAME_DIALOG,"Save Game");
+        PANEL = new NButton(() -> {
             removeFromWorld();
             TURN.start();
         });
-        stats = new TurnStat(players(),TURN.turnNumber);
-        panel.setImage(new GreenfootImage(Appearance.WORLD_WIDTH, Appearance.WORLD_HEIGHT));
+        STATS = new TurnStat(players(),TURN.turnNumber);
+        PANEL.setImage(new GreenfootImage(Appearance.WORLD_WIDTH, Appearance.WORLD_HEIGHT));
         
     }
     
@@ -63,8 +63,8 @@ public class NextTurnPanel {
     private void colorBackground() {
         GColor color = OWNER.color();
         GColor transparentColor = new GColor(color.getRed(),color.getGreen(),color.getBlue(),220);
-        panel.getImage().setColor(transparentColor);
-        panel.getImage().fill();
+        PANEL.getImage().setColor(transparentColor);
+        PANEL.getImage().fill();
     
     }
     
@@ -73,12 +73,12 @@ public class NextTurnPanel {
      */
     private void writeName() {
         if(OWNER.color().luminosity() > 128) {
-            panel.getImage().setColor(GColor.BLACK);
+            PANEL.getImage().setColor(GColor.BLACK);
         } else {
-            panel.getImage().setColor(GColor.WHITE);
+            PANEL.getImage().setColor(GColor.WHITE);
         }
-        panel.getImage().setFont(new Font("monospaced", true, false, 50));
-        panel.getImage().drawString(OWNER.name(), 700, 500);
+        PANEL.getImage().setFont(new Font("monospaced", true, false, 50));
+        PANEL.getImage().drawString(OWNER.name(), 700, 500);
     
     }
     
@@ -87,33 +87,33 @@ public class NextTurnPanel {
      */
     private void writeStats() {
         if(OWNER.color().luminosity() > 128) {
-            panel.getImage().setColor(GColor.BLACK);
+            PANEL.getImage().setColor(GColor.BLACK);
         } else {
-            panel.getImage().setColor(GColor.WHITE);
+            PANEL.getImage().setColor(GColor.WHITE);
         }
         
         String infos = "";
         String armies = "Total number of armies : " 
-                + stats.numberOfArmies.get(OWNER);
+                + STATS.numberOfArmies.get(OWNER);
         String territories = "Number of territories owned : " 
-                + stats.numberOfTerritories.get(OWNER);
+                + STATS.numberOfTerritories.get(OWNER);
         String armiesPerTurn = "Armies in reinforcement this turn : "
-                + (stats.numberOfArmiesPerTurn.get(OWNER) + OWNER.armiesInHand());
+                + (STATS.numberOfArmiesPerTurn.get(OWNER) + OWNER.armiesInHand());
         
         infos += armies + "\n";
         infos += territories + "\n";
         infos += armiesPerTurn + "\n";
         
-        panel.getImage().setFont(new Font("monospaced", true, false, 25));
-        panel.getImage().drawString(infos, 600, 600);
+        PANEL.getImage().setFont(new Font("monospaced", true, false, 25));
+        PANEL.getImage().drawString(infos, 600, 600);
     }
     
     /**
      * Adds the Panel to the World.
      */
     public void addToWorld() {
-        MyWorld.theWorld.addObject(panel, Appearance.WORLD_WIDTH / 2, Appearance.WORLD_HEIGHT / 2);
-        MyWorld.theWorld.addObject(saveButton, Appearance.WORLD_WIDTH - 100, 60);
+        MyWorld.theWorld.addObject(PANEL, Appearance.WORLD_WIDTH / 2, Appearance.WORLD_HEIGHT / 2);
+        MyWorld.theWorld.addObject(SAVE_BUTTON, Appearance.WORLD_WIDTH - 100, 60);
     
     }
     
@@ -121,11 +121,11 @@ public class NextTurnPanel {
      * Removes the Panel from the World.
      */
     private void removeFromWorld() {
-        MyWorld.theWorld.removeObject(panel);
-        MyWorld.theWorld.removeObject(saveButton);
+        MyWorld.theWorld.removeObject(PANEL);
+        MyWorld.theWorld.removeObject(SAVE_BUTTON);
     }
       
-    private Action showSaveGameDialog = ()->{
+    private final Action SHOW_SAVE_GAME_DIALOG = ()->{
         new GameSaver(MyWorld.theWorld.stateManager.game()).askForSaveInfo();
     
     };
@@ -134,29 +134,29 @@ public class NextTurnPanel {
      * Writes the points on the Panel.
      */
     private void writePoints() {
-        panel.getImage().setFont(new Font("monospaced", true, false, 25));
+        PANEL.getImage().setFont(new Font("monospaced", true, false, 25));
         java.awt.Font awtFont = new java.awt.Font("monospaced", java.awt.Font.BOLD, 25);
-        FontMetrics fm = panel.getImage().getAwtImage().getGraphics().getFontMetrics(awtFont);
+        FontMetrics fm = PANEL.getImage().getAwtImage().getGraphics().getFontMetrics(awtFont);
         
         int lineSize = fm.getMaxAscent() + fm.getMaxDescent();
         int baseY = 500;
         int xPos = 10;
         int lineNumber = 1;
         
-        panel.getImage().setColor(GColor.WHITE);
-        panel.getImage().drawString("- Points -", xPos, baseY);
+        PANEL.getImage().setColor(GColor.WHITE);
+        PANEL.getImage().drawString("- Points -", xPos, baseY);
         
         for(Player p : MyWorld.theWorld.stateManager.game().players){
             
             if(!(p instanceof Zombie)){
                 
                 if(p != OWNER){
-                    panel.getImage().setColor(p.color());
+                    PANEL.getImage().setColor(p.color());
                 }else{
-                    panel.getImage().setColor(GColor.WHITE);
+                    PANEL.getImage().setColor(GColor.WHITE);
                 }
                 
-                panel.getImage().drawString(p.name() + " : " + p.points, xPos, baseY + lineNumber * lineSize);
+                PANEL.getImage().drawString(p.name() + " : " + p.points, xPos, baseY + lineNumber * lineSize);
                 lineNumber++;
                 
             }
